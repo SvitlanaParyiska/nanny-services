4acf1ac18138f95c47cb3449daddf0962674dd6a
SvitlanaParyiska <125573969+SvitlanaParyiska@users.noreply.github.com>
2024-03-31 10:33:52 +0000

    Deploying to gh-pages from @ SvitlanaParyiska/nanny-services@65b9a9037df0004db8023f207c53d6e26e264ee9 🚀

---
+++ static/js/405.b73ef549.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknanny_services=self.webpackChunknanny_services||[]).push([[405],{2405:(n,s,e)=>{e.r(s),e.d(s,{default:()=>d});var c=e(1546),a=e(3003),r=e(5043),t=e(1516),i=e(1640),l=e(3775),u=e(579);const d=function(){const n=(0,a.wA)(),s=(0,a.d4)(l.rM);return(0,r.useEffect)((()=>{n((0,i.L)())}),[n]),(0,u.jsx)("main",{className:"nanny-page",children:(0,u.jsxs)("section",{className:"container",children:[(0,u.jsx)(c.A,{}),(0,u.jsx)(t.A,{listToRender:s})]})})}}}]);
+//# sourceMappingURL=405.b73ef549.chunk.js.map
--- static/js/405.f1594035.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknanny_services=self.webpackChunknanny_services||[]).push([[405],{2405:(n,s,e)=>{e.r(s),e.d(s,{default:()=>d});var c=e(1546),a=e(3003),r=e(5043),t=e(8140),i=e(4743),l=e(3775),u=e(579);const d=function(){const n=(0,a.wA)(),s=(0,a.d4)(l.rM);return(0,r.useEffect)((()=>{n((0,i.L)())}),[n]),(0,u.jsx)("main",{className:"nanny-page",children:(0,u.jsxs)("section",{className:"container",children:[(0,u.jsx)(c.A,{}),(0,u.jsx)(t.A,{listToRender:s})]})})}}}]);
-//# sourceMappingURL=405.f1594035.chunk.js.map
